54e1c0188b25d48e489d5fcb9e197213e82c8880
ubendiran2007 <ubendiran2007@gmail.com>
2026-07-20 21:23:47 +0530

    Fix phantom document bug in getAllSectionDocs causing 0 students to be returned

--- backend/utils/studentHelper.js
+++ backend/utils/studentHelper.js
@@ -3,17 +3,18 @@ const { collection, getDocs, db } = require('../firebaseClientWrapper');
 // Helper to fetch all section documents across the DB
 const getAllSectionDocs = async () => {
   const allSectionDocs = [];
-  const batchesSnap = await getDocs(collection(db, 'students'));
+  // Use listDocuments() to find phantom batch documents (documents that only have subcollections)
+  const batchDocsRefs = await db.collection('students').listDocuments();
   
-  for (const batchDoc of batchesSnap.docs) {
-    const depts = await batchDoc.ref.listCollections();
+  for (const batchRef of batchDocsRefs) {
+    const depts = await batchRef.listCollections();
     for (const deptCol of depts) {
       if (deptCol.id === 'departments') continue; // skip old structure if present
       const snap = await deptCol.get();
       snap.docs.forEach(d => allSectionDocs.push({
          ref: d.ref,
          data: d.data(),
-         batch: batchDoc.id,
+         batch: batchRef.id,
          dept: deptCol.id,
          sec: d.id
       }));
